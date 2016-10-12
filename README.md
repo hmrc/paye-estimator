c1ad9dfd17856efc93aec25c8775d8a6d22004f5
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-10-12 16:41:18 +0100

    initial commit
    Project structure and builds

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 # paye-estimator
 
 [![Build Status](https://travis-ci.org/hmrc/paye-estimator.svg?branch=master)](https://travis-ci.org/hmrc/paye-estimator) [ ![Download](https://api.bintray.com/packages/hmrc/releases/paye-estimator/images/download.svg) ](https://bintray.com/hmrc/releases/paye-estimator/_latestVersion)
+[![Scala.js](https://www.scala-js.org/assets/badges/scalajs-0.6.12.svg)](https://www.scala-js.org)
 
-This is a placeholder README.md for a new repository
+Basic PAYE tax calculation library
+
 
 ### License
 
---
+++ project/HmrcBuild.scala
@@ -0,0 +1,52 @@
+import sbt.Keys._
+import sbt._
+import org.scalajs.sbtplugin._
+import ScalaJSPlugin.autoImport._
+
+object HmrcBuild extends Build {
+
+  import uk.gov.hmrc.SbtAutoBuildPlugin
+  import uk.gov.hmrc.versioning.SbtGitVersioning
+  import SbtAutoBuildPlugin._
+  import Dependencies._
+  
+  val nameApp = "paye-estimator"
+
+  lazy val simpleReactiveMongo = Project(nameApp, file("."))
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning, ScalaJSPlugin)
+    .settings(
+      autoSourceHeader := false,
+      scalaVersion := "2.11.8",
+      libraryDependencies ++= Seq(
+
+        //add scala js wrapper ('%%%') libs here 
+
+        Test.scalaTest,
+        Test.pegdown
+      ),
+      resolvers += Resolver.typesafeRepo("releases"),
+      crossScalaVersions := Seq("2.11.8"),
+      //Rhino JS interpreter disabled, Node.js must be installed
+      scalaJSUseRhino := false,
+      //add js libraries
+      skip in packageJSDependencies := false,
+      //minify
+      scalaJSStage := FullOptStage
+    )
+}
+
+object Dependencies {
+
+  object Compile {
+  }
+
+  sealed abstract class Test(scope: String) {
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.0.0" % scope
+    val pegdown = "org.pegdown" % "pegdown" % "1.6.0" % scope
+  }
+
+  object Test extends Test("test")
+
+  object IntegrationTest extends Test("it")
+
+}
