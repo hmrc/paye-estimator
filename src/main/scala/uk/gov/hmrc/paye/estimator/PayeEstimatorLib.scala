48153ffad57a29994c0b8e6205f0d56520e30049
Duncan Crawford <duncan@noughtsandones.co.uk>
2016-10-13 14:55:29 +0100

    WIP class example

--- src/main/scala/uk/gov/hmrc/paye/estimator/PayeEstimatorLib.scala
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmrc.paye.estimator
-
-import scala.scalajs.js.JSApp
-
-//object PayeEstimatorLib extends JSApp{
-//  def main(): Unit = {}
-//}
--- src/main/scala/uk/gov/hmrc/paye/estimator/domain/PayeEstimator.scala
+++ src/main/scala/uk/gov/hmrc/paye/estimator/domain/PayeEstimator.scala
@@ -5,5 +5,11 @@ import scala.scalajs.js.annotation.{JSExport, JSExportAll}
 @JSExport
 object PayeEstimator{
   @JSExport
+  def calculation() = println("1 million pounds")
+}
+
+@JSExport
+class PayeEstimator2{
+  @JSExport
   def calculation() = println("2 million pounds")
 }
