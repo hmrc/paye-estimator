52041fec2a8eeeec4611331d73854b6fc8f6dd52
byronprentice <byron.prentice@digital.hmrc.gov.uk>
2018-02-27 14:46:46 +0000

    NGC-2751 Removed 2016 rates, and 0% bug for NIC rates.

--- src/main/scala/uk/gov/hmrc/payeestimator/domain/TaxCalcSetup.scala
+++ src/main/scala/uk/gov/hmrc/payeestimator/domain/TaxCalcSetup.scala
@@ -54,7 +54,7 @@ trait TaxCalcResource {
 object TaxCalcResourceBuilder {
 
   def resources(isScottish: Boolean): Seq[TaxCalcResource] =
-    Seq(TaxYear_2016_2017(), TaxYear_2017_2018(isScottish), TaxYear_2018_2019(isScottish))
+    Seq(TaxYear_2017_2018(isScottish), TaxYear_2018_2019(isScottish))
 
   def resourceForDate(date:LocalDate, isScottish: Boolean) : TaxCalcResource = {
     resources(isScottish).find(_.applicableForDate(date)) getOrElse {
@@ -89,7 +89,7 @@ case class TaxYear_2018_2019(isScottish:Boolean = false) extends TaxCalcResource
   val threshold1 = RateLimit("primary", 8424.00)
   val threshold2 = RateLimit("secondary", 8424.00)
 
-  val employee1 = RateLimit("1", 12)// why 12% not zero?
+  val employee1 = RateLimit("1", 0)
   val employee2 = RateLimit("2", 0)
   val employee3 = RateLimit("3", 12)
   val employee4 = RateLimit("4", 2)
@@ -150,43 +150,3 @@ case class TaxYear_2017_2018(isScottish:Boolean = false) extends TaxCalcResource
     Seq(employer1, employer2, employer3, employer4)
   )
 }
-
-case class TaxYear_2016_2017(isScottish:Boolean = false) extends TaxCalcResource {
-
-  override val emergencyTaxCode: String = "1100L"
-  override val startDate: LocalDate = LocalDate.of(2016,4,6)
-  override val endDate: LocalDate = LocalDate.of(2017,4,5)
-
-  val taxBands1 = Band(1, BigDecimal(0.00), 10, PeriodCalc( "annual", 0, 0, 0))
-  val taxBands2 = Band(2, BigDecimal(3000.00), 20, PeriodCalc( "annual", 32000.00, 6400.00, 6400.00))
-  val taxBands3 = Band(3, BigDecimal(118000.00), 40, PeriodCalc("annual", 150000.00, 53600.00, 47200.00))
-  val taxBands4 = Band(4, BigDecimal(-1), 45, PeriodCalc( "annual", -1, -1, -1))
-
-  override val taxBands = TaxBands(BigDecimal(100000.00), BigDecimal(10), BigDecimal(50), Seq(taxBands1,taxBands2,taxBands3,taxBands4))
-
-
-  val rateLimit1 = RateLimit("lower", 5824.00)
-  val rateLimit2 = RateLimit("upper", 43000.00)
-
-  val threshold1 = RateLimit("primary", 8060.00)
-  val threshold2 = RateLimit("secondary", 8112.00)
-
-  val employee1 = RateLimit("1", 12)
-  val employee2 = RateLimit("2", 0)
-  val employee3 = RateLimit("3", 12)
-  val employee4 = RateLimit("4", 2)
-
-  val employer1 = RateLimit("1", 0)
-  val employer2 = RateLimit("2", 13.8)
-  val employer3 = RateLimit("3", 13.8)
-  val employer4 = RateLimit("4", 0)
-
-  override val nicRateLimits = NICRateLimits(
-    Seq(rateLimit1, rateLimit2),
-    Seq(threshold1, threshold2),
-    Seq(employee1, employee2, employee3, employee4),
-    Seq(employer1, employer2, employer3, employer4)
-  )
-}
-
-
